21419530fb05f439da800481876b1a6b850dc5f6
Alec Keller <alec.keller.13@cnu.edu>
2017-02-02 21:38:02 -0500

    added php files

--- app/src/main/java/com/aleckeller/deliverit/AppConfig.java
+++ app/src/main/java/com/aleckeller/deliverit/AppConfig.java
@@ -5,8 +5,8 @@ package com.aleckeller.deliverit;
  */
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.0.14:8888/deliverIt_api/login.php";
+    public static String URL_LOGIN = "http://192.168.1.162/deliverIt_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.0.14:8888/deliverIt_api/register.php";
+    public static String URL_REGISTER = "http://192.168.1.162/deliverIt_api/register.php";
 }
